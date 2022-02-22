7818b746556d3eba4408997d3a0f2cea25e8cd78
Lesley159 <lesleyhope159@gmail.com>
2022-02-22 13:59:10 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
 
     NEWS_API_KEY_URL = 'https://newsapi.org/v2/sources?apiKey=13ced76b35ac4200a34b1ac5b97cae7d'
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    NEWS_API_KEY = os.environ.get(13ced76b35ac4200a34b1ac5b97cae7d)
 
     NEWS_API_SOURCES_URL='http://newsapi.org/v2/top-headlines?''country=us&''apiKey=13ced76b35ac4200a34b1ac5b97cae7d'
     
